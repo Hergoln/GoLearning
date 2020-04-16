91949c23c03b58131c0f4cc8e148db03cf84d750
Hergoln <juliusz.malka@o2.pl>
2020-04-16 09:10:50 +0200

    added FunctionsAndDerivatives package

--- FunctionsAndDerivatives/ReLu.go
+++ FunctionsAndDerivatives/ReLu.go
@@ -6,9 +6,9 @@ func ReLuFunc(x float64) float64 {
 	return math.Max(0, x)
 }
 
-func RLFuncDeriv(x float64) float64 {
+func ReLuFuncDeriv(x float64) float64 {
 	if x <= 0 {
 		return 0
 	}
-	return x
+	return 1
 }
